4d0cd50cc294d5a78124ee348ae0b1beb7308ebf
Murtaza <Murtaza_taloo@hotmail.com>
2020-06-17 17:22:27 +1000

    initial commit

---
+++ src/components/reactComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ExampleReact = () => (
+  <div>
+    <h1>this is the example react component</h1>
+  </div>
+);
+export default ExampleReact;
---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <h2>Oh yes</h2>
+    <ExampleReact/>
+    <p>This is the TodoList app v1.0.0. It is part of the Vue crash course on Youtube</p>
+  </div>
+</template>
+
+<script>
+import { ReactInVue } from "vuera";
+// import ExampleReact from "../components/reactComponent.react";
+
+// console.log("ER...", ExampleReact);
+
+export default {
+  name: "myComponent",
+  components: { ExampleReact },
+};
+</script>
